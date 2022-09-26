f824e7fb1a145c30a288a8637409d51964a78543
HuanyuLee <hadoop_2020@163.com>
2022-09-26 20:00:44 +0800

    AC FrameArrangement

--- algorithm-class/Code01_FrameArrangement.cpp
+++ algorithm-class/Code01_FrameArrangement.cpp
@@ -1,26 +1,52 @@
 // https://acm.ecnu.edu.cn/contest/563/problem/A/
 // Created by HuanyuLee on 2022/9/24.
-//
+// AC
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+int binarySearchFirstMaxNumber(const int x[], int n, int ans) {
+    int l = 0;
+    int r = n - 1;
+    int index = -1;
+    while (l <= r) {
+        int mid = (r - l) / 2 + l;
+        if (x[mid] > ans) {
+            index = mid;
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return index;
+}
+
+int getMinimumQuantityK(const int x[], int n, int k) {
+    int count = 1;
+    int i = 0;
+    int ans = x[i] + k;
+    while (i < n) {
+        if (ans >= x[n - 1]) {
+            break;
+        }
+        // find next
+        i = binarySearchFirstMaxNumber(x, n, ans);
+        ans = x[i] + k;
+        count++;
+    }
+    return count;
+}
+
 int main() {
-    int n,k;
+    int n, k;
     cin >> n >> k;
-    int coordinates[n+1];   // coordinates[0] save the answer
-    for (int i = 1; i <= n; ++i) {
-        cin >> coordinates[i];
+    int x[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> x[i];
     }
-
-    int a = 123;
-    string sa = to_string(a);
-
-    // for test
-    for (int i = 0; i < n+1; ++i) {
-        cout << coordinates[i] << " ";
-    }
-
+    sort(x, x + n);
+    cout << getMinimumQuantityK(x, n, k);
     return 0;
 }
