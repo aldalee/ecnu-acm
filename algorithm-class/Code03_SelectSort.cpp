79e5391ac29159a5e9e237c99ce50500cd55afc4
HuanyuLee <hadoop_2020@163.com>
2022-10-14 19:43:28 +0800

    AC SelectSort

---
+++ algorithm-class/Code03_SelectSort.cpp
@@ -0,0 +1,14 @@
+// https://acm.ecnu.edu.cn/contest/563/problem/C/
+// Created by HuanyuLee on 2022/10/14.
+// AC
+#include <iostream>
+using namespace std;
+
+int main(){
+    long n = 100000;
+    cout << n << endl;
+    int arr[n];
+    for (int i = n; i > 0; --i) {
+        cout << i << " ";
+    }
+}
