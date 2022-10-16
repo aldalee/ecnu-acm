657e70dfb6177175c03ae90f7b3ac4c818b9ba48
HuanyuLee <hadoop_2020@163.com>
2022-10-16 19:11:30 +0800

    AC DifferentialCounting

--- algorithm-class/Code05_DifferentialCounting.cpp
+++ algorithm-class/Code05_DifferentialCounting.cpp
@@ -1,21 +1,26 @@
 // https://acm.ecnu.edu.cn/contest/563/problem/E/
 // Created by HuanyuLee on 2022/10/13.
-// not AC
+// AC
 #include <iostream>
-#include <unordered_map>
 
 using namespace std;
-
+#define MAX_VALUE 3000000
+#define MAX_LENGTH 7000000
+// hashCount[]的索引代表原数组arr[]的值，hashCount[i]的值代表arr[i]出现的次数
+// 因为arr[i]有正有负，因此做这样的处理
+// hash(x) = MAX_VALUE + x    x ∈ [-MAX_VALUE,MAX_VALUE]
+// 查找 x 的位置，时间复杂度O(1)
+long hashCount[MAX_LENGTH];
 int read() {
     int x = 0, w = 1;
     char c = 0;
     while (c < '0' || c > '9') {
         if (c == '-') w = -1;
-        c = getchar();
+        c = (char)getchar();
     }
     while (c >= '0' && c <= '9') {
         x = x * 10 + (c - '0');
-        c = getchar();
+        c = (char)getchar();
     }
     return x * w;
 }
@@ -23,21 +28,20 @@ int read() {
 int main() {
     int n, x;
     cin >> n >> x;
-    int arr[n];
+    long diff[n];
+    long count = 0;
     for (int i = 0; i < n; ++i) {
-        arr[i] = read();
+        int num = read();
+        // a[i] - a[j] = x 等价于 a[i] - x = a[j]
+        // 求差值数组
+        diff[i] = num - x;
+        // 将数num映射到hash表中
+        hashCount[num + MAX_VALUE]++;
     }
-    unordered_map<int, int> hashtable;
+    // 统计有序对的个数
     for (int i = 0; i < n; ++i) {
-        hashtable[arr[i] - x] = i;
+        count += hashCount[diff[i] + MAX_VALUE];
     }
-    int count = 0;
-    for (int i = 0; i < n; ++i) {
-        auto it = hashtable.find(arr[i]);
-        if (it != hashtable.end()) {
-            count++;
-        }
-    }
-    cout << count;
+    printf("%ld", count);
     return 0;
 }
